1d890b29cb13c4faa5dbc82943107f6dac96337a
Xon <tazipper1@hotmail.com>
2015-08-20 13:01:46 +0800

    Ensure installer removes the old version

--- upload/library/SV/ThreadPostBBCode/Installer.php
+++ upload/library/SV/ThreadPostBBCode/Installer.php
@@ -5,6 +5,20 @@ class SV_ThreadPostBBCode_Installer
     public static function install($installedAddon, array $addonData, SimpleXMLElement $xml)
     {
         $db = XenForo_Application::get('db');
+
+        $addonModel = XenForo_Model::create("XenForo_Model_AddOn");
+        $addonsToUninstall = array('SVBBThreadPost');
+        foreach($addonsToUninstall as $addonToUninstall)
+        {
+            $addon = $addonModel->getAddOnById($addonToUninstall);
+            if (!empty($addon))
+            {
+                $dw = XenForo_DataWriter::create('XenForo_DataWriter_AddOn');
+                $dw->setExistingData($addonToUninstall);
+                $dw->delete();
+            }
+        }
+
         // truncate the bbcode cache table instead of slowly deleting bits
         $db->query('
             TRUNCATE TABLE xf_bb_code_parse_cache
